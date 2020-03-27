f7dbe14a3a2b105c91d0f90faf6c012ecb7984b6
Dmitry Pimenov <dpim@fb.com>
2020-03-26 21:50:25 -0700

    updated build info

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,7 @@
+const webpack = require('webpack');
+const isProd = (process.env.NODE_ENV || 'production') === 'production';
+const assetPrefix = isProd ? '/dpim.github.io' : '';
+
 module.exports = {
     exportTrailingSlash: true,
     exportPathMap: () => {
@@ -5,11 +9,19 @@ module.exports = {
             '/': { page: '/' }
         };
     },
+    assetPrefix: assetPrefix,   
     webpack: (config) => {
-        config.module.rules.push({
-            test: /\.md$/,
-            use: 'raw-loader'
-        });
+        config.module.rules.push(
+            {
+                test: /\.md$/,
+                use: 'raw-loader'
+            }
+        );
+        config.plugins.push(
+            new webpack.DefinePlugin({
+              'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix)
+            })
+        );
         return config;
     }
 }
